55074c9fecda4cddca85db5bcdbd7aed1a90bb4e
Caio Augusto <caio@attingedigital.com.br>
2023-08-15 16:21:05 -0300

    feat: init microservice

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { HandlersModule } from './handlers/handlers.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [HandlersModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
